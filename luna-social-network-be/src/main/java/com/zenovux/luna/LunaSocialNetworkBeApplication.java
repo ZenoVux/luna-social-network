e5901579f2798102ac45b9d8d9ba42d6c5941333
ZenoVux <zenovux0702@gmail.com>
2023-06-15 22:52:43 +0700

    Create backend project

---
+++ luna-social-network-be/src/main/java/com/zenovux/luna/LunaSocialNetworkBeApplication.java
@@ -0,0 +1,13 @@
+package com.zenovux.luna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunaSocialNetworkBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunaSocialNetworkBeApplication.class, args);
+    }
+
+}
---
+++ luna-social-network-be/src/test/java/com/zenovux/luna/LunaSocialNetworkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenovux.luna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunaSocialNetworkBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
